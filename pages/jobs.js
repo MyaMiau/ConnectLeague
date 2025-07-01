09b0d3558963402403639cf82722334ec48379d8
Mya <zeusmeuhusky@gmail.com>
2025-06-30 21:35:01 -0300

    arrumei o jobs

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,8 +1,4 @@
-// src/pages/api/jobs.js
-
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '@/lib/prisma';
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
